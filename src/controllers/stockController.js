b87de43dba9136a65e2d32affc9ee5de71ab3980
Raj <blcc.raj@gmail.com>
2023-12-19 21:46:13 +0530

    feat: Add timestamp to stock updates and alerts

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -5,15 +5,27 @@ import { isValidPrice, isValidTicker } from "../utils/validator.js";
 const WAIT_INTERVAL = 1000;
 
 async function waitForTargetPrice(ticker, targetPrice, socket) {
+  const startTime = Date.now();
   let currentPrice = await getStockData(ticker);
 
   while (currentPrice < targetPrice) {
-    socket.emit("stockUpdate", { ticker, currentPrice });
+    const elapsedTime = Date.now() - startTime;
+    const formattedTime = new Date(elapsedTime).toISOString().substring(11, 8);
+    socket.emit("stockUpdate", {
+      ticker,
+      currentPrice,
+      formattedTime,
+    });
     await sleep(WAIT_INTERVAL);
     currentPrice = await getStockData(ticker);
   }
 
-  return currentPrice;
+  return {
+    currentPrice,
+    formattedTime: new Date(Date.now() - startTime)
+      .toISOString()
+      .substring(11, 19),
+  };
 }
 
 function sleep(ms) {
@@ -27,13 +39,13 @@ const stockController = {
         return res.status(400).json({ error: "Invalid ticker or price" });
       }
 
-      const currentPrice = await waitForTargetPrice(
+      const { currentPrice, formattedTime } = await waitForTargetPrice(
         ticker,
         parseFloat(price),
         req.app.get("io")
       );
 
-      const alertMessage = sendAlert(ticker, price);
+      const alertMessage = sendAlert(ticker, price, formattedTime);
 
       req.app.get("io").emit("stockAlert", { ticker, alertMessage });
       return res.json({ currentPrice, alert: alertMessage });
--- src/services/alertService.js
+++ src/services/alertService.js
@@ -1,9 +1,10 @@
-function sendAlert(ticker, price) {
+function sendAlert(ticker, price, timestamp) {
   const alterMessage = {
     type: "stock-alert",
     ticker: ticker,
     price,
     message: `The stock ${ticker} has reached the target price of ${price}.`,
+    timestamp,
   };
   console.log(alterMessage);
   return alterMessage;
