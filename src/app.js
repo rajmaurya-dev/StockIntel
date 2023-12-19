2ec5102c137e9ee1c06fee34cdb70f0d7b7c4afe
Raj <blcc.raj@gmail.com>
2023-12-19 18:19:31 +0530

    feat: Add socket.io functionality for real-time stock updates

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 import express from "express";
+import http from "http";
 import bodyParser from "body-parser";
 import stockRoutes from "./routes/stockRoute.js";
+import { Server } from "socket.io";
+
 const app = express();
 const PORT = 5000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 //Middleware
 app.use(bodyParser.json());
 app.use("/api", stockRoutes);
+
+//Socket.io
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.emit("message", "Welcome to StockIntel");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+app.set("io", io);
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -2,6 +2,23 @@ import { sendAlert } from "../services/alertService.js";
 import { getStockData } from "../services/stockDataService.js";
 import { isValidPrice, isValidTicker } from "../utils/validator.js";
 
+const WAIT_INTERVAL = 1000;
+
+async function waitForTargetPrice(ticker, targetPrice, socket) {
+  let currentPrice = await getStockData(ticker);
+
+  while (currentPrice < targetPrice) {
+    socket.emit("stockUpdate", { ticker, currentPrice });
+    await sleep(WAIT_INTERVAL);
+    currentPrice = await getStockData(ticker);
+  }
+
+  return currentPrice;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 const stockController = {
   async getStock(req, res) {
     try {
@@ -10,12 +27,16 @@ const stockController = {
         return res.status(400).json({ error: "Invalid ticker or price" });
       }
 
-      const currentPrice = await getStockData(ticker);
-      if (currentPrice >= parseFloat(price)) {
-        const alertMessage = sendAlert(ticker, price);
-        return res.json({ currentPrice, alert: alertMessage });
-      }
-      return res.json({ currentPrice });
+      const currentPrice = await waitForTargetPrice(
+        ticker,
+        parseFloat(price),
+        req.app.get("io")
+      );
+
+      const alertMessage = sendAlert(ticker, price);
+
+      req.app.get("io").emit("stockAlert", { ticker, alertMessage });
+      return res.json({ currentPrice, alert: alertMessage });
     } catch (error) {
       console.log(error);
       res.status(500).json({ error: "Internal Server Error" });
